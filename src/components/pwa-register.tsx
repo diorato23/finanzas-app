5ebd9b0a65d65c72b8c9ac6b44a95117a51bb3cc
diorato23 <diorato@live.com>
2026-03-10 10:54:08 -0500

    fix: update middleware and sw registration logic

--- src/components/pwa-register.tsx
+++ src/components/pwa-register.tsx
@@ -4,7 +4,7 @@ import { useEffect } from "react"
 
 export function ServiceWorkerRegister() {
     useEffect(() => {
-        if ("serviceWorker" in navigator && window.location.protocol === "https:" || window.location.hostname === "localhost") {
+        if ("serviceWorker" in navigator && (window.location.protocol === "https:" || window.location.hostname === "localhost")) {
             window.addEventListener("load", () => {
                 navigator.serviceWorker
                     .register("/sw.js")
